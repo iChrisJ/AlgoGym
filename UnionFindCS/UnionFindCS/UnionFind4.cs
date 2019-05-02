cd81c7611331418c01674042a04facd7854773f2
Chis J <chrisjiang@outlook.com>
2019-05-02 22:40:31 +0800

    UnionFind implementation in C#

---
+++ UnionFindCS/UnionFindCS/UnionFind4.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+
+namespace UF4
+{
+	class UnionFind
+	{
+		private int count;
+		private int[] parent;
+		private int[] rank;
+
+		public UnionFind(int count)
+		{
+			this.count = count;
+			this.parent = new int[this.count];
+			this.rank = new int[this.count];
+			for (int i = 0; i < this.count; i++)
+			{
+				parent[i] = i;
+				rank[i] = 1;
+			}
+
+		}
+
+		public int Find(int p)
+		{
+			Debug.Assert(p >= 0 && p < this.count);
+
+			while (p != parent[p])
+				p = parent[p];
+
+			return p;
+		}
+
+		public bool IsConnected(int p, int q)
+		{
+			return this.Find(p) == this.Find(q);
+		}
+
+		public void Union(int p, int q)
+		{
+			int pRoot = this.Find(p);
+			int qRoot = this.Find(q);
+
+			if (pRoot == qRoot)
+				return;
+
+			if (rank[pRoot] < rank[qRoot])
+			{
+				parent[pRoot] = qRoot;
+			}
+			else if (rank[pRoot] > rank[qRoot])
+			{
+				parent[qRoot] = pRoot;
+			}
+			else //rank[pRoot] == rank[qRoot]
+			{
+				parent[pRoot] = qRoot;
+				rank[qRoot]++;
+			}
+
+		}
+	}
+}
