5b878143dbfe3319c924041d729c82260783eab4
iChrisJ <chrisjiang@outlook.com>
2019-06-03 23:28:44 +0800

    mergesort and mergesort buttom up

---
+++ SortingAdvanced/SortingAdvanced/MergeSort.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+
+#include "InsertionSort.h"
+
+// 将arr[l...mid]和arr[mid+1...r]两部分进行归并
+template<typename T>
+void __merge(T arr[], int l, int mid, int r)
+{
+	T *aux = new T[r - l + 1];
+	for (int i = l; i <= r; i++)
+		aux[i - l] = arr[i];
+
+	int i = l, j = mid + 1;
+	for (int k = l; k <= r; k++)
+	{
+		if (i > mid)
+		{
+			arr[k] = aux[j - l];
+			j++;
+		}
+		else if (j > r)
+		{
+			arr[k] = aux[i - l];
+			i++;
+		}
+		else if (aux[i - l] < aux[j - l])
+		{
+			arr[k] = aux[i - l];
+			i++;
+		}
+		else
+		{
+			arr[k] = aux[j - l];
+			j++;
+		}
+	}
+}
+
+// 递归使用归并排序,对arr[l..r]的范围进行排序
+template<typename T>
+void __mergeSort(T arr[], int l, int r)
+{
+	/*if (l >= r)
+		return;*/
+	// 优化2: 对于小规模数组, 使用插入排序
+	if (r - l <= 15) {
+		insertionSort(arr, l, r);
+		return;
+	}
+
+	int mid = (l + r) / 2;
+	__mergeSort(arr, l, mid);
+	__mergeSort(arr, mid + 1, r);
+
+	// 优化1: 对于arr[mid] <= arr[mid+1]的情况,不进行merge
+	// 对于近乎有序的数组非常有效,但是对于一般情况,有一定的性能损失
+	if (arr[mid] > arr[mid + 1])
+		__merge(arr, l, mid, r);
+}
+
+template<typename T>
+void mergeSort(T arr[], int n)
+{
+	__mergeSort(arr, 0, n - 1);
+}
---
+++ SortingAdvanced/SortingAdvanced/SortingAdvanced.cpp
@@ -0,0 +1,59 @@
+﻿// SortingAdvanced.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <cassert>
+#include "SortTestHelper.h"
+#include "InsertionSort.h"
+#include "MergeSort.h"
+#include "MergeSortBU.h"
+
+int main()
+{
+	int n = 100000;
+
+	// 测试1 一般性测试
+	cout << "Test for random array, size = " << n << ", random range [0, " << n << "]" << endl;
+	int* arr1 = SortTestHelper::generateRandomArray(n, 0, n);
+	int* arr2 = SortTestHelper::copyIntArray(arr1, n);
+	int* arr3 = SortTestHelper::copyIntArray(arr1, n);
+
+	SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
+	SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
+	SortTestHelper::testSort("Merge Sort Buttom Up", mergeSortBU, arr3, n);
+
+	delete[] arr1;
+	delete[] arr2;
+	delete[] arr3;
+
+	cout << endl;
+
+	// 测试2 测试近乎有序的数组
+	// 对于近乎有序的数组, 数组越有序, InsertionSort的时间性能越趋近于O(n)
+	// 所以可以尝试, 当swapTimes比较大时, MergeSort更快
+	// 但是当swapTimes小到一定程度, InsertionSort变得比MergeSort快
+	int swapTimes = 1000;
+	assert(swapTimes >= 0);
+
+	cout << "Test for nearly ordered array, size = " << n << ", swap time = " << swapTimes << endl;
+	arr1 = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
+	arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+	SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
+	SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
+
+	delete[] arr1;
+	delete[] arr2;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
