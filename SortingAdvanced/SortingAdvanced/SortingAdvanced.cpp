4abd8f2ea14d5e308d32e3917a4dcad032cc6bae
iChrisJ <chrisjiang@outlook.com>
2019-06-05 23:26:11 +0800

    Quick Sort 3 Ways

--- SortingAdvanced/SortingAdvanced/SortingAdvanced.cpp
+++ SortingAdvanced/SortingAdvanced/SortingAdvanced.cpp
@@ -9,10 +9,11 @@
 #include "MergeSort.h"
 #include "MergeSortBU.h"
 #include "QuickSort.h"
+#include "QuickSort3Ways.h"
 
 int main()
 {
-	int n = 100000;
+	int n = 1000000;
 
 	// 测试1 一般性测试
 	cout << "Test for random array, size = " << n << ", random range [0, " << n << "]" << endl;
@@ -20,16 +21,19 @@ int main()
 	int* arr2 = SortTestHelper::copyIntArray(arr1, n);
 	int* arr3 = SortTestHelper::copyIntArray(arr1, n);
 	int* arr4 = SortTestHelper::copyIntArray(arr1, n);
+	int* arr5 = SortTestHelper::copyIntArray(arr1, n);
 
 	//SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
 	SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
 	//SortTestHelper::testSort("Merge Sort Buttom Up", mergeSortBU, arr3, n);
 	SortTestHelper::testSort("Quick Sort", quickSort, arr4, n);
+	SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr5, n);
 
 	delete[] arr1;
 	delete[] arr2;
 	delete[] arr3;
 	delete[] arr4;
+	delete[] arr5;
 
 	cout << endl;
 
@@ -44,10 +48,27 @@ int main()
 	arr1 = SortTestHelper::generateNearlyOrderedArray(n, swapTimes);
 	arr2 = SortTestHelper::copyIntArray(arr1, n);
 	arr3 = SortTestHelper::copyIntArray(arr1, n);
+	arr4 = SortTestHelper::copyIntArray(arr1, n);
 
 	//SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
 	SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
 	SortTestHelper::testSort("Quick Sort", quickSort, arr3, n);
+	SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr4, n);
+
+	delete[] arr1;
+	delete[] arr2;
+	delete[] arr3;
+	delete[] arr4;
+
+	// 测试3 测试存在包含大量相同元素的数组
+	cout << "Test for random array, size = " << n << ", random range [0,10]" << endl;
+	arr1 = SortTestHelper::generateRandomArray(n, 0, 10);
+	arr2 = SortTestHelper::copyIntArray(arr1, n);
+	arr3 = SortTestHelper::copyIntArray(arr1, n);
+
+	SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+	SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+	SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr3, n);
 
 	delete[] arr1;
 	delete[] arr2;
